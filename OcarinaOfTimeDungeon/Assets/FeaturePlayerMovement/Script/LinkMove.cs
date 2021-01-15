5d5a53ac630fe2a9bc041593c750e1b941753520
Bob-Hoogenboom <marjabob910@gmail.com>
2021-01-15 09:29:33 +0100

    Jump and Gravity
    +added a basic jump
    +added gravity to the player object
    
    ~changed movement a bit for the jump mechanic

--- OcarinaOfTimeDungeon/Assets/FeaturePlayerMovement/Script/LinkMove.cs
+++ OcarinaOfTimeDungeon/Assets/FeaturePlayerMovement/Script/LinkMove.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class LinkMove : MonoBehaviour
 {
+    [SerializeField] private Vector3 _playerVelocity;
+    [SerializeField] private bool _groundedPlayer;
     [SerializeField] private float _speed = 6f;
     [SerializeField] private float _turnTime = 0.1f;
+    [SerializeField] private float _jumpHeight = 1.0f;
+    [SerializeField] private float _gravityScale = -9.81f;
 
+    private CharacterController charCon;
     private float _smoothTurn;
 
-    public CharacterController charCon;
+    void Start()
+    {
+        charCon = GetComponent<CharacterController>();
+    }
 
-   
 
     void Update()
     {
@@ -19,9 +26,28 @@ public class LinkMove : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
 
-        if(direction.magnitude >=0.1f)
+
+        _groundedPlayer = charCon.isGrounded;
+        if (_groundedPlayer && _playerVelocity.y < 0)
+        {
+            _playerVelocity.y = 0f;
+        }
+
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && _groundedPlayer)
+        {
+            _playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3.0f * _gravityScale);
+        }
+
+        _playerVelocity.y += _gravityScale * Time.deltaTime;
+
+
+
+        //smooth turning
+        if (direction.magnitude >=0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _smoothTurn, _turnTime);
@@ -29,5 +55,8 @@ public class LinkMove : MonoBehaviour
 
             charCon.Move(direction * _speed * Time.deltaTime);
         }
+
+        charCon.Move(_playerVelocity * Time.deltaTime);
+
     }
 }
