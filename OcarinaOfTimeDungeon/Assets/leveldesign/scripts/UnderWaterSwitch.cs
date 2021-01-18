81ac3b8b47a5834a25a6ea010c1f2f99393553c9
Mark Ulrich <31822@ma-web.nl>
2021-01-18 15:55:29 +0100

    Added water and platform down movement on key held down

---
+++ OcarinaOfTimeDungeon/Assets/leveldesign/scripts/UnderWaterSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderWaterSwitch : MonoBehaviour
+{
+    [SerializeField] private float _moveToLocationY;
+    private Vector3 waterlow;
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        waterlow = new Vector3(transform.position.x, _moveToLocationY, transform.position.z);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waterlow, Time.deltaTime * speed);
+        }
+    }
+}
