f6d6ef902e3ed21db6ace1b47db349adeca71ad1
Jason Dikken <electronicjason.dikken@gmail.com>
2021-01-18 13:21:56 +0100

    #8
    -fixed health/rupee scriptnames
    -fixed prefabs

--- OcarinaOfTimeDungeon/Assets/featureUserInterface/Scripts/UIRupee.cs
+++ OcarinaOfTimeDungeon/Assets/featureUserInterface/Scripts/UIRupee.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RupeeCounter : MonoBehaviour
+public class UIRupee : MonoBehaviour
 {
     public Text text;
     [SerializeField] private int rupees = 4;
