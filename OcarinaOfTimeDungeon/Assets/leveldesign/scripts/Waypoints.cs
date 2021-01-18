58b9595c011f6090534e55c33cf78972190aec10
Mark Ulrich <31822@ma-web.nl>
2021-01-18 12:21:53 +0100

    Added platform movement script, updated level and platform prefabs

---
+++ OcarinaOfTimeDungeon/Assets/leveldesign/scripts/Waypoints.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public GameObject[] _waypoints;
+    int current = 0;
+    public float speed;
+    float WPradius = 1;
+
+    void Update()
+    {
+        if (Vector3.Distance(_waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= _waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, _waypoints[current].transform.position, Time.deltaTime * speed);
+
+    }
+}
