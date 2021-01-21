8ad82555fc716c7eb2451fb41fddb53366708532
Mark Ulrich <31822@ma-web.nl>
2021-01-21 11:05:59 +0100

    Added WIP underwater switch

---
+++ OcarinaOfTimeDungeon/Assets/leveldesign/scripts/SwitchCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchCollision : MonoBehaviour
+{
+    public UnityEvent waterDown;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            Debug.Log("event dmg function");
+            waterDown.Invoke(); //assign the waterdown function
+        }
+    }
+}
--- OcarinaOfTimeDungeon/Assets/leveldesign/scripts/UnderWaterSwitch.cs
+++ OcarinaOfTimeDungeon/Assets/leveldesign/scripts/UnderWaterSwitch.cs
@@ -7,17 +7,33 @@ public class UnderWaterSwitch : MonoBehaviour
     [SerializeField] private float _moveToLocationY;
     private Vector3 waterlow;
     [SerializeField] private float speed;
+    //[SerializeField] private GameObject waterSwitch;
+
+    bool isPushed = false;
 
     void Start()
     {
         waterlow = new Vector3(transform.position.x, _moveToLocationY, transform.position.z);
     }
 
-    void Update()
+    //void Update()
+    //{
+    //    OnTriggerEnter(waterSwitch.GetComponent<MeshCollider>());
+    //}
+
+    public void MoveWaterDown()
     {
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position = Vector3.MoveTowards(transform.position, waterlow, Time.deltaTime * speed);
-        }
+        transform.position = Vector3.MoveTowards(transform.position, waterlow, Time.deltaTime * speed);
     }
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    Debug.Log(col);
+    //    //if (!isPushed)
+    //    //{
+    //    //    isPushed = true;
+            
+    //    //}
+    //    gameObject.transform.position = Vector3.MoveTowards(transform.position, waterlow, Time.deltaTime * speed);
+    //}
 }
